510b0d5eb6275f849d3ba592e1543865fdb0d608
Tara Harwood <tara.m.harwood@gmail.com>
2021-03-08 16:43:41 -0800

    database fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_08_215114) do
+ActiveRecord::Schema.define(version: 2021_03_09_002911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,10 +23,10 @@ ActiveRecord::Schema.define(version: 2021_03_08_215114) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
-    t.string "type"
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "category"
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
