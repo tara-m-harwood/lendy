192a3a5bb2210792945b51aae9c0eecf8f676e01
Tara Harwood <tara.m.harwood@gmail.com>
2021-03-11 14:27:38 -0800

    fix item redirect

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -18,7 +18,7 @@ class ItemsController < ApplicationController
     def update
         @item = Item.find(params[:id])
         @item.update(item_params)
-        render json: @item
+        redirect_back
     end
 
     def destroy
